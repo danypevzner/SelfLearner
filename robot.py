33f108a79ed4d168b609f2cbb9dee3fb280a1af0
erogodsky <erogodsky93@gmail.com>
2023-02-16 21:51:30 +0300

    Новый сенсор, получение точек

--- robot.py
+++ robot.py
@@ -1,6 +1,7 @@
 import time
 from zmqRemoteApi import RemoteAPIClient
 
+
 class Robot:
     SPEED_BOOST = 1
 
@@ -10,23 +11,29 @@ class Robot:
         def init_sensors():
             _ = self._get_sensor_data(self._sensor)
 
-
         self.name = robot_name
         self.sim = sim
-        self._sensor = self.sim.getObject('/' + self.name + '/FRONTSENS')
-        init_sensors()
+        # self._sensor = self.sim.getObject('/' + self.name + '/FRONTSENS')
+        # init_sensors()
 
-    def _get_sensor_data(self, sensor):
-        sensor_output = ['result',
-                         'distance',
-                         'detectedPoint',
-                         'detectedObjectHandle',
-                         'detectedSurfaceNormalVector']
-        return {sensor_output[i]: sim.readProximitySensor(sensor)[i] for i in range(len(sensor_output))}
+    # def _get_sensor_data(self, sensor):
+    #     sensor_output = ['result',
+    #                      'distance',
+    #                      'detectedPoint',
+    #                      'detectedObjectHandle',
+    #                      'detectedSurfaceNormalVector']
+    #     return {sensor_output[i]: sim.readProximitySensor(sensor)[i] for i in range(len(sensor_output))}
 
-    def _wall_detection(self):
-        return self._get_sensor_data(self._sensor)['distance'], self._get_sensor_data(self._sensor)['result']
+    def _get_sensor_data(self):
+        string_signal = sim.getStringSignal("measuredDataAtThisTime")
+        points = sim.unpackTable(string_signal)
+        two_d_points = [[pt[0], pt[1]] for pt in points]
+        return two_d_points
 
+    # def _wall_detection(self):
+    #     return self._get_sensor_data(self._sensor)['distance'], \
+    #            self._get_sensor_data(self._sensor)['result'],\
+    #            self._get_sensor_data(self._sensor)['detectedPoint']
 
     def _set_movement(self, forward_back_vel, left_right_vel, rotation_vel):
         wheel_joints = [sim.getObject('/' + self.name + '/rollingJoint_fl'),
@@ -38,16 +45,16 @@ class Robot:
         sim.setJointTargetVelocity(wheel_joints[2], -forward_back_vel - left_right_vel + rotation_vel)
         sim.setJointTargetVelocity(wheel_joints[3], -forward_back_vel + left_right_vel + rotation_vel)
 
-
     def start(self):
         while 1:
-            distance, result = self._wall_detection()
-            print( result, distance)
-            if result and (distance < 0.3):
-                self._set_movement(0, 0, 0)
-                break
-            else:
-                self._set_movement(self.SPEED_BOOST * 1, 0, 0)
+            # distance, result, point = self._wall_detection()
+            # print(point)
+            # if result and (distance < 0.8):
+            #     self._set_movement(0, 0, 0)
+            #     break
+            # else:
+            self._set_movement(self.SPEED_BOOST * 1, 0, 0)
+            print(self._get_sensor_data())
             # print(self._get_sensor_data(self._sensor))
 
 
