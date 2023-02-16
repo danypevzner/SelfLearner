4e280ecc366acfb7fd4c30e1e28a34f442923b42
erogodsky <erogodsky93@gmail.com>
2023-02-16 22:56:32 +0300

    Получение дистанций, остановка на расстоянии X

--- robot.py
+++ robot.py
@@ -4,12 +4,13 @@ from zmqRemoteApi import RemoteAPIClient
 
 class Robot:
     SPEED_BOOST = 1
+    X_DISTANCE = 0.8
 
     def __init__(self, sim, robot_name):
         self._start_time = time.time()
 
-        def init_sensors():
-            _ = self._get_sensor_data(self._sensor)
+        # def init_sensors():
+        #     _ = self._get_sensor_data(self._sensor)
 
         self.name = robot_name
         self.sim = sim
@@ -25,10 +26,20 @@ class Robot:
     #     return {sensor_output[i]: sim.readProximitySensor(sensor)[i] for i in range(len(sensor_output))}
 
     def _get_sensor_data(self):
-        string_signal = sim.getStringSignal("measuredDataAtThisTime")
-        points = sim.unpackTable(string_signal)
-        two_d_points = [[pt[0], pt[1]] for pt in points]
-        return two_d_points
+        result = sim.getInt32Signal("r")
+
+        if result == 1:
+            point_signal = sim.getStringSignal("pointData")
+            points = sim.unpackTable(point_signal)
+            points_2d = [[pt[0], pt[1]] for pt in points]
+
+            dist_signal = sim.getStringSignal("distData")
+            dists = sim.unpackTable(dist_signal)
+        else:
+            points_2d = []
+            dists = []
+
+        return result, points_2d, dists
 
     # def _wall_detection(self):
     #     return self._get_sensor_data(self._sensor)['distance'], \
@@ -47,15 +58,16 @@ class Robot:
 
     def start(self):
         while 1:
-            # distance, result, point = self._wall_detection()
-            # print(point)
-            # if result and (distance < 0.8):
-            #     self._set_movement(0, 0, 0)
-            #     break
-            # else:
             self._set_movement(self.SPEED_BOOST * 1, 0, 0)
-            print(self._get_sensor_data())
-            # print(self._get_sensor_data(self._sensor))
+
+            result, points, dists = self._get_sensor_data()
+            # print(points)
+            # print(dists)
+
+            distance = min(dists)
+            if result and (distance < self.X_DISTANCE):
+                self._set_movement(0, 0, 0)
+                break
 
 
 client = RemoteAPIClient()
